c659652027098cc968339aea4d24381e34b2c119
lealucas <78903864+lealucas@users.noreply.github.com>
2021-05-25 00:26:57 +0200

    pp

--- connexion.php
+++ connexion.php
@@ -15,8 +15,10 @@
         if ($db_found) {
             if ($email != "") {
             $sql = "SELECT * FROM Client WHERE email LIKE '%$email%'";
+            $sql2 = "SELECT * FROM Vendeur WHERE email LIKE '%$email%'";
                 if ($Mdp != "") {
                 $sql .= " AND Mdp LIKE '%$Mdp%'";
+                $sql2 .= " AND Mdp LIKE '%$Mdp%'";
                 }
                 else{
                     $bug = true;
@@ -26,8 +28,9 @@
                 $bug = true;
             }
             $result = mysqli_query($db_handle, $sql);
+            $result2 = mysqli_query($db_handle, $sql2);
             
-            if (mysqli_num_rows($result) == 0 || $bug ==true) {
+            if (mysqli_num_rows($result) == 0 || $bug == true ) {
             echo "Email ou mot de passe incorrect";
             } else {
                 while ($data = mysqli_fetch_assoc($result)) {

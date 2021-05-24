b0b17c8338b2b5b6b6e47ea38c0829be3a58813c
lealucas <78903864+lealucas@users.noreply.github.com>
2021-05-24 18:19:26 +0200

    suite

--- votreCompte.php
+++ votreCompte.php
@@ -1,37 +1,37 @@
 <?php
     $email = isset($_POST["email"])?$_POST["email"]:"";
     $Password = isset($_POST["Password"])?$_POST["Password"]:"";
+    $Btn = isset($_POST["Btn"])?$_POST["Btn"]:""; 
 
-    $found = false;
+    $error = false;
     echo "<table border=\"1\">";
     echo "<tr>";
     echo "<h2>Connectez-vous </h2>";
-    echo "<td>email:</td>";
+    echo "<td>Email:</td>";
     echo "<td><input type=\"text\" name=\"email\"></td>";
     echo "</tr>";
+    echo "<td>Mot de passe:</td>";
+    echo "<td><input type=\"text\" name=\"Password\"></td>";
+    echo "</tr>";
+    echo "<td colspan=\"2\" align=\"center\">";
+    echo "<input type=\"submit\" name=\"Btn\" value=\"Soumettre\">";
+    echo "</td>";
+    echo "</tr>";
     
+    if($email=='' || $Password==''){
+        $error = true;
+    }
+    if($error=false){
     for($i = 0; $i < count($logs); $i++){
-        if($client[$email] == $email){
+        if($client[$email] == $email && $client[$Password] == $Password){
             $connection = true;
         }
-        elseif ($client[$Password]==$Password) {
+        elseif ($vendeur[$email]==$email && $vendeur[$Password] == $Password) {
             $connection = true;
         }
-        else{
-            echo"Connexion refusée. Adresse mail ou mot de passe incorrect.";
-        }
+        else{}
     }
-        
+}
+    
 
-    if (!$found) {
-        echo"Connexion refusée. utilisateur inconnu.";
-    }
-    else{
-        if ($connection) {
-            echo"Connexion okay";
-        }
-        else{
-            echo"Connexion refusée. Mot de passe invalide";
-        }
-    }
 ?>

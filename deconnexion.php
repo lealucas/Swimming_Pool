de4e0b88b01e88416fdfc5b76497d266469a81af
FlorianSez <florian.sezille@edu.ece.fr>
2021-05-26 16:12:12 +0200

    Bon chance, je valide, avec validation, Joker je ferme les yeux

---
+++ deconnexion.php
@@ -0,0 +1,59 @@
+<?php
+    session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesAccueil.css" >
+    <title>Document</title>
+</head>
+<body>
+
+    <div id="wrapper">
+        <!-- Header -->
+        <div id="header">
+                <div id="barreheader">
+                    Bienvenue : (Identifiant)
+                    <a href="" class="lienh" STYLE="padding:0 0 0 790px">Admin</a> |
+                    <a href="inscription.html" class="lienh">S'inscrire</a> | 
+                    <a href="" class="lienh">Mon compte</a> | 
+                    <a href="connexion.php" class="lienh">Se connecter</a>
+                </div>
+        </div>
+
+
+        <!-- Menu de navigation -->
+        <div id="nav">
+            <h1>ECE MarketPlace</h1>
+            
+            <ul id="menuNav">
+                Recherche : <input type="text" name="" id="" placeholder="Tapez un nom d'article...">
+                <a href="index.html" class="lienn">Accueil</a>
+                <a href="toutParcourir.html" class="lienn">Tout Parcourir</a>
+                <a href="Notifications.html" class="lienn">Notifications</a>
+                <a href="Panier.html" class="lienn">Panier</a>
+                <a href="connexion.php" class="lienn">Votre Compte</a>
+            </ul>
+        </div>
+
+
+        <div id="section">
+            <div id="position">
+                <p id="position_texte">Vous êtes bien déconnecté, merci de votre passage.</p>
+            </div>
+        </div>
+
+
+        <!-- Footer -->
+        <footer>
+            <small>
+                Copyright &copy; 2021, Sezille_Lucas_-_Industry<br>
+                <a href="mailto:sezille_lucas@gmail.com">sezille_lucas@gmail.com</a>
+            </small>
+        </footer>
+    </div>
+</body>
+</html>
